3953852532ce4820af4d7e236c84a3e2d860da38
Nemoredd <nemored123@bk.ru>
2024-06-30 10:40:49 +0700

    added programm

---
+++ Operations/Model.java
@@ -0,0 +1,24 @@
+package Operations;
+
+public class Model implements Model1{
+
+    Double x;
+    Double y;
+    
+
+    @Override
+    public Double result() {
+        return 0.0;
+    }
+
+    @Override
+    public void setX(Double value) {
+        
+    }
+
+    @Override
+    public void setY(Double value) {
+        
+    }
+    
+}
